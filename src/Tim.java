b885a5c20ffab42d934ec0ed0052e321300fe986
tryskii <770simon.migas770@azet.sk>
2017-01-14 17:13:13 +0100

    Spravil som triedu tim iba som tam nahdzal metody caakam na Kikov konstruktor .....

---
+++ src/Tim.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Šimon on 14.01.2017.
+ */
+public class Tim
+{
+    public void nastavBody(int Body)
+    {
+
+    }
+    public void nastavPocetSpravnych()
+    {
+
+    }
+    public void nastavPocetNespravnych()
+    {
+
+    }
+    public void pripocitajBodyZaSpravne(int spravne)
+    {
+
+    }
+    public void pripocitajBodyZaNespravne(int nespravne)
+    {
+
+    }
+    public void vypisNazovTimu()
+    {
+
+    }
+    public void info()
+    {
+
+    }
+}

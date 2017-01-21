e4c3d21e3e1f3e09718eb9b18829991821e91e8b
niznikpeter <otep968@gmail.com>
2017-01-21 15:44:29 +0100

    version 1.0.1.1 //added methods giveQuestion & addPoints version 1.0.1.2 //should be done :) one more thing left: fill fields

--- src/Question.java
+++ src/Question.java
@@ -4,6 +4,9 @@ import java.util.Random;
  * Created by otep9 on 13.01.2017.
  */
 public class Question {
+    public int random = 50;
+    public int tmp = 50;
+
     private String questionsDAT[] = new String[50];
     private String answersDAT[] = new String[50];
 
@@ -24,36 +27,76 @@ public class Question {
 
     public void pickQuestion(int a) {
         Random rn = new Random();
-        int ran = rn.nextInt(50);
+        int ran = rn.nextInt(random);
         switch (a) {
             case 1:                                    //vyberie nahodnu otazku z temy SXT
                 System.out.println(questionsSXT[ran]);
+                System.out.println("Správna odpoveď je: ");
+                System.out.println(answersSXT[ran]);
+                doNotRepeatQuestion(ran, questionsSXT);
+                doNotRepeatAnswer(ran, answersSXT);
+                break;
             case 2:                                    //vyberie nahodnu otazku z temy SIE
                 System.out.println(questionsSIE[ran]);
-            case 3:                                    //vyberie nahodnu otazku z temy PRO
+                System.out.println("Správna odpoveď je: ");
+                System.out.println(answersSIE[ran]);
+                doNotRepeatQuestion(ran, questionsSIE);
+                doNotRepeatAnswer(ran, answersSIE);
+                break;
+            case 3:                                    //vyberie nahodnu otazku z temy PAY
                 System.out.println(questionsPAY[ran]);
-            case 4:                                    //vyberie nahodnu otazku z temy PAY
-                System.out.println(questionsPAY[ran]);
+                System.out.println("Správna odpoveď je: ");
+                System.out.println(answersPAY[ran]);
+                doNotRepeatQuestion(ran, questionsPAY);
+                doNotRepeatAnswer(ran, answersPAY);
+                break;
+            case 4:                                    //vyberie nahodnu otazku z temy PRO
+                System.out.println(questionsPRO[ran]);
+                System.out.println("Správna odpoveď je: ");
+                System.out.println(answersPRO[ran]);
+                doNotRepeatQuestion(ran, questionsPRO);
+                doNotRepeatAnswer(ran, answersPRO);
+                break;
             case 5:                                    //vyberie nahodnu otazku z temy AEN
                 System.out.printf(questionsAEN[ran]);
+                System.out.println("Správna odpoveď je: ");
+                System.out.println(answersAEN[ran]);
+                doNotRepeatQuestion(ran, questionsAEN);
+                doNotRepeatAnswer(ran, answersAEN);
+                break;
             case 6:                                    //vyberie nahodnu otazku z temy DAT
                 System.out.printf(questionsDAT[ran]);
+                System.out.println("Správna odpoveď je: ");
+                System.out.println(answersDAT[ran]);
+                doNotRepeatQuestion(ran, questionsDAT);
+                doNotRepeatAnswer(ran, answersDAT);
+                break;
             default:
                 System.out.println("Wrong input!!");
+                break;
         }
-    }
-    public void checkAnswer(){
-
-    }
-    public void doNotRepeatQuestion() {
+        random--;
 
     }
 
-    public void giveQuestion() {
-
+    public void doNotRepeatQuestion(int ran, String s[]) {
+        String temp = s[ran];
+        s[ran] = s[tmp];
+        s[tmp] = temp;
+        tmp--;
     }
 
-    public void addPoints() {
+    public void doNotRepeatAnswer(int ran, String s[]) {
+        String temp = s[ran];
+        s[ran] = s[tmp];
+        s[tmp] = temp;
+        tmp--;
+    }
 
+    public int addPoints(int i) {
+        if (i == 1)
+            return 5;
+        else
+            return -5;
     }
 }
